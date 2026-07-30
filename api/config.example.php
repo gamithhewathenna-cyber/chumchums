a20b3cfae80cdbca1a2f92ee3cc45b7df8ffac0c
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-07-30 21:40:36 +0530

    DP

--- api/config.example.php
+++ api/config.example.php
@@ -4,7 +4,7 @@ return [
   'db_host'    => 'localhost',
   'db_name'    => 'creaeina_chumspos',     // the DB you create in cPanel
   'db_user'    => 'creaeina_chumsposadmim create in cPanel
-  'db_pass'    => '!XuP^R]x9+vm]6^C
+  'db_pass'    => '!XuP^R]x9+vm]6^C'
   'jwt_secret' => 'CHANGE_THIS_to_a_long_random_string',
   'jwt_ttl'    => 43200,                // token lifetime in seconds (12h)
 ];
