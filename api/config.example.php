5a5a54988a150ee162768738cb2b8ce9d7f404a5
gamithhewathenna-cyber <gamithhewathenna@gmail.com>
2026-07-30 21:11:27 +0530

    DB

--- api/config.example.php
+++ api/config.example.php
@@ -2,9 +2,9 @@
 // Copy this file to config.php and fill in your cPanel MySQL details.
 return [
   'db_host'    => 'localhost',
-  'db_name'    => 'cpaneluser_pos',     // the DB you create in cPanel
-  'db_user'    => 'cpaneluser_posuser', // the DB user you create in cPanel
-  'db_pass'    => 'your_db_password',
+  'db_name'    => 'creaeina_chumspos',     // the DB you create in cPanel
+  'db_user'    => 'creaeina_chumsposadmim create in cPanel
+  'db_pass'    => '!XuP^R]x9+vm]6^C
   'jwt_secret' => 'CHANGE_THIS_to_a_long_random_string',
   'jwt_ttl'    => 43200,                // token lifetime in seconds (12h)
 ];
